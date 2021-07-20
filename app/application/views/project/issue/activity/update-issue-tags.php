a2bcd493aa88aa86f34f3376189e3dc542d4c6a8
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-07-20 10:25:47 -0400

    Corrections mineures qui deraient arranger un bug révélé dans les tags

--- app/application/views/project/issue/activity/update-issue-tags.php
+++ app/application/views/project/issue/activity/update-issue-tags.php
@@ -13,10 +13,12 @@
 						$Col = ($activity->attributes['parent_id'] == $activity->attributes['item_id'] ) ? "none" : "underline";
 						$TagNum = Tag::where('id', '=', $activity->attributes['action_id'] )->first(array('id','tag','bgcolor','ftcolor'));
 						$Who = \User::where('id', '=', $activity->attributes['user_id'] )->get(array('firstname','lastname','email'));
-						echo '<label style="background-color: '.@$TagNum->attributes['bgcolor'].';color: '.@$TagNum->attributes['ftcolor'].'; padding: 5px 10px; border-radius: 8px;">'.
-						@$TagNum->attributes['tag'].'</label> : <span style="font-weight: bold; text-decoration: '.$Col.';">'.$Msg.
-						'</span> '.__('tinyissue.by') . ' <b>' .$Who[0]->attributes["firstname"].' '.$Who[0]->attributes["lastname"].'</b> '.
-						' '.date(Config::get('application.my_bugs_app.date_format'), strtotime($activity->attributes['updated_at']));
+						echo '<label style="background-color: '.@$TagNum->attributes['bgcolor'].';color: '.@$TagNum->attributes['ftcolor'].'; padding: 5px 10px; border-radius: 8px;">';
+						echo @$TagNum->attributes['tag'];
+						echo '</label> : ';
+						echo '<span style="font-weight: bold; text-decoration: '.$Col.';">'.$Msg.'</span>';
+						echo  __('tinyissue.by') . ' <b>' .$Who[0]->attributes["firstname"].' '.$Who[0]->attributes["lastname"].'</b> ';
+						echo date(Config::get('application.my_bugs_app.date_format'), strtotime($activity->attributes['updated_at']));
 					} else {
 						$j = json_decode($activity->attributes['data'], true);
 						echo count($j['added_tags']).' '.((count($j['added_tags']) > 1) ? __('tinyissue.tags_added') : __('tinyissue.tag_added')).'.';
