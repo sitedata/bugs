4f4573c2f8e80e9fdf127146122111e2780d896a
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-09-12 18:45:18 -0400

    Les étiquettes fonctionnent bien

--- app/application/views/activity/update-issue-tags.php
+++ app/application/views/activity/update-issue-tags.php
@@ -19,20 +19,20 @@
 					echo '&nbsp;<strong>'.$user->firstname . ' ' . $user->lastname.'</strong>&nbsp;';
 				} 
 			
-			if($tag_counts['added'] > 0 && $tag_counts['removed'] > 0) { echo '<div class="tag-activity-spacer"></div>'; } 
+				if($tag_counts['added'] > 0 && $tag_counts['removed'] > 0) { echo '<div class="tag-activity-spacer"></div>'; } 
 			
-			if($tag_counts['removed'] > 0) {
-				foreach($tag_diff['removed_tags'] as $tag) { 
-					echo '<label class="label notice" style="color:'.$tag_diff['tag_data'][$tag]['ftcolor'].';background-color:'.$tag_diff['tag_data'][$tag]['bgcolor'].';">' . $tag_diff['tag_data'][$tag]['tag'] . '</label>'; 
-				}
-				echo __($tag_counts['removed'] > 1 ? 'tinyissue.tags_removed' : 'tinyissue.tag_removed'); 
+				if($tag_counts['removed'] > 0) {
+					foreach($tag_diff['removed_tags'] as $tag) { 
+						echo '<label class="label notice" style="color:'.$tag_diff['tag_data'][$tag]['ftcolor'].';background-color:'.$tag_diff['tag_data'][$tag]['bgcolor'].';">' . $tag_diff['tag_data'][$tag]['tag'] . '</label>'; 
+					}
+					echo __($tag_counts['removed'] > 1 ? 'tinyissue.tags_removed' : 'tinyissue.tag_removed'); 
+						echo ' '; 
+					echo __('tinyissue.in'); 
 					echo ' '; 
-				echo __('tinyissue.in'); 
-					echo ' '; 
-				echo '<a href="'.$issue->to().'">'.$issue->title.'</a>';
-				echo __('tinyissue.by');
-				echo '<strong>'.$user->firstname . ' ' . $user->lastname.'</strong>';
-			} 
+					echo '<a href="'.$issue->to().'">'.$issue->title.'</a>&nbsp;';
+					echo __('tinyissue.by'); 
+					echo '&nbsp;<strong>'.$user->firstname . ' ' . $user->lastname.'</strong>&nbsp;';
+				} 
 			?>
 		</div>
 		<span class="time">
